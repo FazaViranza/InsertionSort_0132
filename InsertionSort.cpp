88d708895ef936aadf75f47e2ef9eb0dbf0efd71
FazaViranza <nofila.faza.ft24@mail.umy.ac.id>
2025-03-25 23:57:11 +0700

    Commit

---
+++ InsertionSort.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int arr [20]; // Membuat Array dengan panjang data 20
+int n; // Membuat Variable inputan n
+
+void input()
+{ //Procedure Input
+while (true)
+{
+cout << "Masukkan Jumlah Data pada Array : "; // Membuat Inputan Jumlah element Array
+cin >> n; //memanggil variable inputan n
+
+if (n <= 20)
+{ // Membuat Kondisi n tidak lebih dari 20
+break;
+}
+else
+{
+cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n"; // Menampilkan Pesan jika data lebih dari 20 
+}
+}
+    }
